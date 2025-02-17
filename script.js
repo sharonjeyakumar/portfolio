fbcb64cf36ded9df35f1878beea0db4481d085cf
Sharon Jeyakumar <159943912+sharonjeyakumar@users.noreply.github.com>
2025-02-17 21:39:02 +0530

    refined both desktop and mobile view

--- script.js
+++ script.js
@@ -20,36 +20,69 @@ const aboutNav = document.querySelector('.aboutNav');
 
 
 window.addEventListener('scroll', ()=>{
-    console.log(scrollY);
-    if(window.scrollY<260){
-        directory.textContent = 'home';
-    } 
-    
-    if (window.scrollY>260){
-        directory.textContent = 'projects';
-    }
+    // console.log(scrollY);
 
-    if(window.scrollY>788){
-        directory.textContent = 'experience';
+    if(window.innerWidth >= 500){
+        if(window.scrollY<260){
+            directory.textContent = 'home';
+        } 
+        
+        if (window.scrollY>260){
+            directory.textContent = 'projects';
+        }
+    
+        if(window.scrollY>788){
+            directory.textContent = 'experience';
+        }
+        
+        if(window.scrollY>1095){
+            directory.textContent = 'education';
+        } 
+    
+        if(window.scrollY>1478){
+            directory.textContent = 'about';
+        }
+    } else {
+        if(window.scrollY<233){
+            directory.textContent = 'home';
+        } 
+        
+        if (window.scrollY>233){
+            directory.textContent = 'projects';
+        }
+    
+        if(window.scrollY>785){
+            directory.textContent = 'experience';
+        }
+        
+        if(window.scrollY>1055){
+            directory.textContent = 'education';
+        } 
+    
+        if(window.scrollY>1385){
+            directory.textContent = 'about';
+        }
     }
     
-    if(window.scrollY>1095){
-        directory.textContent = 'education';
-    } 
-
-    if(window.scrollY>1478){
-        directory.textContent = 'about';
-    }
 })
 
 
 //Navigation Buttons
 homeNav.addEventListener('click', ()=>{
-    window.scrollTo(0,0);
+    if(window.innerWidth >500){
+        window.scrollTo(0,0);
+    } else {
+        window.scrollTo(0,0);
+    }
+    
    
 })
 projectNav.addEventListener('click', ()=>{
-    window.scrollTo(0,300);
+    if(window.innerWidth >500){
+        window.scrollTo(0,300);
+    } else {
+        window.scrollTo(0,270);
+    }
     
 })
 // experienceNav.addEventListener('click', ()=>{
@@ -59,7 +92,11 @@ projectNav.addEventListener('click', ()=>{
 //     window.scrollTo(0,820)
 // })
 aboutNav.addEventListener('click', ()=>{
-    window.scrollTo(0,1510)
+    if(window.innerWidth >500){
+        window.scrollTo(0,1510);
+    } else {
+        window.scrollTo(0,1410);
+    }
 })
 
 //Nav Bar
